613b4a217098dc7015a0cd75f15ae915c2ea5da6
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2022-04-30 22:39:01 -0400

    Update misc.py

--- util/misc.py
+++ util/misc.py
@@ -268,7 +268,7 @@ def get_sha():
 
 def collate_fn(batch):
     batch = list(zip(*batch))
-    # batch[0] = nested_tensor_from_tensor_list(batch[0])
+    batch[0] = nested_tensor_from_tensor_list(batch[0])
     return list(batch)
 
 
