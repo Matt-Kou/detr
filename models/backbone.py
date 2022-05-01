e104545650bc3d6e47c56160baf1d1b1d0bba031
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2022-04-30 23:43:05 -0400

    Update backbone.py

--- models/backbone.py
+++ models/backbone.py
@@ -89,7 +89,7 @@ class Backbone(BackboneBase):
         backbone = getattr(torchvision.models, name)(
             replace_stride_with_dilation=[False, False, dilation],
             pretrained=is_main_process(), norm_layer=FrozenBatchNorm2d)
-        backbone.load_state_dict(torch.load("zoo/backbone/resnet.pth"))
+        # backbone.load_state_dict(torch.load("zoo/backbone/resnet.pth"))
         num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
