f36c375edfc805a06b4e16e93f47fe4ac883ce2b
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2022-04-30 20:34:45 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -25,6 +25,7 @@ def train_one_epoch(model: torch.nn.Module,
     print_freq = 10
 
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+        print(samples, targets)
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
